86c74b597a22809b367f7756b9c85e33dd6babd2
Roderick <enuke4@gmail.com>
2022-01-18 22:24:05 -0600

    build the DayListItem components & modify the stories/index.js file - run storybook to check the output - achieved the expected output

---
+++ src/components/DayListItem.js
@@ -0,0 +1,27 @@
+import React from "react";
+import classnames from "classnames";
+
+export default function DayListItem(props) {
+
+  const dayClass = classnames("day-list__item", {
+    "dayl-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+
+  // const spotsRemaining = (spots) => {
+  //   if (spots === 0) {
+  //     return "Sorry - no spots left.";
+  //   } else if (spots === 1) {
+  //     return "There is 1 more spot left.";
+  //   } else {
+  //     return `There is/are ${spots} spot/s remaining.`;
+  //   }
+  // }
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{props.spots}</h3>
+    </li>
+  );
+}
