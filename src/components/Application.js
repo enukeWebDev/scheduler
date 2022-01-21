a2a69555e27be614363b610e181f8ecc1526dcba
Roderick <enuke4@gmail.com>
2022-01-20 21:07:37 -0600

    add the appoinement component to the actual application - expected result achieved

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,27 +4,97 @@ import "components/Application.scss";
 
 import DayList from "components/DayList";
 
+import Appointment from "components/Appointment";
+
+// const [day, setDay] = useState('Monday');
+// console.log(day);
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
+
+
 export default function Application(props) {
 
-  const [day, setDay] = useState('Monday');
-  console.log(day);
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+  // const [day, setDay] = useState('Monday');
+  // console.log(day);
+  // const days = [
+  //   {
+  //     id: 1,
+  //     name: "Monday",
+  //     spots: 2,
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Tuesday",
+  //     spots: 5,
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Wednesday",
+  //     spots: 0,
+  //   },
+  // ];
+
+  const [day, setDay] = useState("Monday");
+
+  const schedules = appointments.map(appointment => {
+    return (
+      <Appointment key={appointment.id} {...appointment} />
+    )
+  })
 
   return (
     <main className="layout">
@@ -50,7 +120,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {schedules}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
