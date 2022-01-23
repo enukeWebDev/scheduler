538743462cdc09e2b4d0d689683c6063afbe79c3
Roderick <enuke4@gmail.com>
2022-01-23 12:53:09 -0600

    build a custom hook that allows to manage the visual mode of any component

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,25 @@
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (newMode, replace = false) => {
+    if (replace) {
+      history.pop();
+    }
+    history.push(newMode);
+    setMode(newMode);
+    setHistory(history);
+  }
+
+  const back = () => {
+    if (history.length > 1) {
+      history.pop();
+      setHistory(history);
+      setMode(history[history.length - 1]);
+    } else {
+      setMode(mode);
+    }
+  }
+
+  return { mode, transition, back };
+}
